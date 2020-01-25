a0fca4377e62c0a6657b27fe9fa70ea1b5b283a3
Willi Rath <wrath@geomar.de>
2020-01-25 12:39:53 +0100

    Implement dataframe capturing

---
+++ capture_qstatall_to_csv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import click
+import pandas as pd
+from io import StringIO
+import subprocess
+
+
+def capture_qstat_now():
+    """Create dataframe from qstatall, minus sensitive info, plus timestamp."""
+
+    # get timestamp before the (possibly slow) parsing happens
+    time_stamp = pd.TImestamp.now()
+
+    # call qstat and capture stdout stream
+    qstat_cmd = ['qstatall', ]
+    qstat_stdout = subprocess.Popen(qstat_cmd, stdout=subprocess.PIPE)
+    qstat_file_like = StringIO(qstat_stdout.communicate()[0].decode('utf-8'))
+
+    # read into pd dataframe, drop sensitive info, and remove separator row
+    df = pd.read_csv(qstat_file_like, delim_whitespace=True)
+    df = df.drop(columns=["RequestID", "ReqName", "UserName"])
+    df = df.drop(df.index[0])
+
+    # set time stamp
+    df['time'] = time_stamp
+
+    return df
+
+
+@click.command()
+@click.argument("file_name")
+def write_df_to_disk(file_name):
+    """Dump qstatall dataframe to `file_name`."""
+    df = capture_qstat_now()
+    df.to_csv(file_name)
+
+
+if __name__ == '__main__':
+    write_df_to_disk()
