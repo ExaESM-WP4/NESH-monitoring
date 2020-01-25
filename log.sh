c1df4a456378e5e120eb46ba9d6a993508a23914
Willi Rath <wrath@geomar.de>
2020-01-25 12:30:02 +0100

    Add qstat capturing to driver script

--- log.sh
+++ log.sh
@@ -4,10 +4,13 @@ cd log_files
 
 # Log CPU and memory usage for Nesh execution hosts.
 
+mkdir -p qstat_logs/
+
 target_sec=$(date -d "+7 days" +%s) # Total output period
 
 while [ $[$target_sec-$(date +%s)] -gt 0 ]; do
  now=$(date +"%Y-%m-%d-%H-%M-%S")
  qstat -E -F fcpu,fmem1,ucpu,cpuavg1,umem1,ehost,quenm -o fcpu > ${now}.log
+ capture_qstatall_to_csv.py --filename qstat_logs/qstatall_${now}.csv.gz
  sleep 10m # Output interval
 done
