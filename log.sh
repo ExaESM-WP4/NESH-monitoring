6d0ac6bfdaa28308f36ffa01ab420753d9a1a33f
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2020-02-04 19:27:39 +0100

    Shorten log file name

--- log.sh
+++ log.sh
@@ -33,6 +33,6 @@ salt_value=$(uuidgen -r) # Random UUID as salt value
 while [ $[$target_sec-$(date +%s)] -gt 0 ]; do
  now=$(date +"%Y-%m-%d-%H-%M-%S")
  qstat -E -F fcpu,fmem1,fswap1,ucpu,umem1,uswap1,cpuavg1,ldavg1,ehost,quenm -o fcpu > host_logs/${now}.log
- ./capture_qstatall_to_csv.py ${salt_value} request_logs/qstatall_${now}.csv.gz
+ ./capture_qstatall_to_csv.py ${salt_value} request_logs/${now}.csv.gz
  sleep 10m # Output interval
 done
