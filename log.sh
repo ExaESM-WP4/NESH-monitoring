4f03e3605677a594633a88a07fc58f7bd5bdf5f2
Willi Rath <willirath@users.noreply.github.com>
2020-02-04 15:52:01 +0100

    Drop unnecessary explicit python call

--- log.sh
+++ log.sh
@@ -31,6 +31,6 @@ salt_value=$(uuidgen -r) # Random UUID as salt value
 while [ $[$target_sec-$(date +%s)] -gt 0 ]; do
  now=$(date +"%Y-%m-%d-%H-%M-%S")
  qstat -E -F fcpu,fmem1,ucpu,cpuavg1,umem1,ehost,quenm -o fcpu > ${now}.log
- python3 ../capture_qstatall_to_csv.py ${salt_value} qstat_logs/qstatall_${now}.csv.gz
+../capture_qstatall_to_csv.py ${salt_value} qstat_logs/qstatall_${now}.csv.gz
  sleep 10m # Output interval
 done
