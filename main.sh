8cd847620a1dfa0c6aaa3b9ab350e3bd0aea34d6
Willi Rath <willirath@users.noreply.github.com>
2020-02-06 19:32:50 +0100

    Adjust file names and directory structure (#7)
    * Change log directory names
    
    * Shorten log file name
    
    * Shorten file name
    
    * Remove obsolete qstat output sorting
    
    * Directly compress qstat log files
    
    * Do tar.gz on uncompressed logs instead
    
    * Clean up existing log cycle file structure
    
    * Delete as now obsolete
    
    * Add tar.gz functionality and run all cells
    
    * Remove as now obsolete
    
    * Choose more informative file name
    
    * Define longer output period
    
    Co-authored-by: Katharina Höflich <37177103+kathoef@users.noreply.github.com>

---
+++ main.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Steps to create a clean env
+#
+# module load python3.7.4
+# rm -rf py3venv/
+# python3 -m venv py3venv/
+# source py3venv/bin/activate
+# pip install click pandas
+# pip freeze > requirements.txt
+
+# Initialize virtual Python environment.
+
+module load python3.7.4 # Python base version
+which python3 &> /dev/null || { echo "python3 not found"; exit 1; }
+
+if [ -d py3venv/ ]; then
+ source py3venv/bin/activate
+else
+ python3 -m venv py3venv/
+ source py3venv/bin/activate
+ pip install -r requirements.txt
+fi
+
+# Log CPU and memory usage for Nesh execution hosts.
+
+mkdir -p host_logs/
+mkdir -p request_logs/
+
+target_sec=$(date -d "+91 days" +%s) # Total output period
+salt_value=$(uuidgen -r) # Random UUID as salt value
+
+while [ $[$target_sec-$(date +%s)] -gt 0 ]; do
+ now=$(date +"%Y-%m-%d-%H-%M-%S")
+ qstat -E -F fcpu,fmem1,fswap1,ucpu,umem1,uswap1,cpuavg1,ldavg1,ehost,quenm > host_logs/${now}.log
+ ./qstatall.py ${salt_value} request_logs/${now}.csv
+ sleep 10m # Output interval
+done
+
+# Log file grouped compression.
+
+cd host_logs
+first=`ls *.log | head -1`
+tar czf cycle_${first:0:19}.tar.gz *.log && rm *.log
+
+cd ../request_logs
+first=`ls *.csv | head -1`
+tar czf cycle_${first:0:19}.tar.gz *.csv && rm *.csv
